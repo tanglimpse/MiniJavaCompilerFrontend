4b9d1811bacee83fdab1a6250cf4c3df38bd95a8
窗明 <tanglimpse@chuangmingjijingdeMacBook-Pro.local>
2019-01-05 21:23:01 +0800

    Add test file.

---
+++ src/tests/syntax-test.java
@@ -0,0 +1,17 @@
+class SyntaxError {
+    pubic stratic voip sjfaklaja(Str[] args) {
+        System.out.println(new O().run())
+    }
+}
+
+class O {
+    public int run( {
+        Aoifjwijefijaasafsafjwliewkejwlkjkefjiw
+    }
+
+    public     f1() { return 1; }
+
+    public int   () { return 2; }
+
+           int f3() { return 3; }
+}
