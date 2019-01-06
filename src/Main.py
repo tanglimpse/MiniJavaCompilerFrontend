a693f3c7971fd297dd55a4e1da7515ef5233f75e
窗明 <tanglimpse@chuangmingjijingdeMacBook-Pro.local>
2019-01-06 13:12:30 +0800

    Generate target tree.

--- src/Main.py
+++ src/Main.py
@@ -1,15 +1,21 @@
 from antlr4 import *
+from MiniJavaLexer import MiniJavaLexer
+from MiniJavaParser import MiniJavaParser
+from MiniJavaVisitor import MiniJavaVisitor
+from MiniJavaListener import MiniJavaListener
 import sys
 
 
-def main():
+def main(argv):
 
-    print ("Firstly get the input file")
-    if len(sys.argv) != 2:
-        print ("Command Wrong!")
-    
-    input = FileStream(sys.argv[1])
+    print ("First get the input file")
+    texts = FileStream(argv[1])
+    lexer = MiniJavaLexer(texts)
+    stream = CommonTokenStream(lexer)
+    parser = MiniJavaParser(stream)
+    tree = parser.goal()
+    print(tree.toStringTree(recog=parser))
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
     
